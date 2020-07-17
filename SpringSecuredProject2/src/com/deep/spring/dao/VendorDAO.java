50d988514e8f1f78467485dd6cd3e31f16ecb61b
Deep-codebase <deepjyoti.dhiraj.das>
2020-07-17 21:32:15 +0530

    xml approach

---
+++ SpringSecuredProject2/src/com/deep/spring/dao/VendorDAO.java
@@ -0,0 +1,14 @@
+package com.deep.spring.dao;
+
+import java.util.List;
+
+import com.deep.spring.entity.Vendor;
+
+public interface VendorDAO {
+
+	public List<Vendor> loadAllVendors();
+
+	public void saveVendor(Vendor vendor);
+	
+	public List<Vendor> getVendorDetails();
+}
---
+++ SpringSecuredProject2/src/com/deep/spring/dao/VendorDAOImpl.java
@@ -0,0 +1,63 @@
+package com.deep.spring.dao;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.transaction.Transactional;
+
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.deep.spring.entity.Customer;
+import com.deep.spring.entity.Vendor;
+
+@Repository
+public class VendorDAOImpl implements VendorDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public List<Vendor> loadAllVendors() {
+		return null;
+	}
+
+	public void saveVendor(Vendor vendor) {
+		Session session = sessionFactory.getCurrentSession();
+		session.save(vendor);
+		System.out.println("Vendor saved");
+	}
+
+	public List<Vendor> getVendorDetails() {
+		Session currentSession = sessionFactory.getCurrentSession();
+
+//		Query<Vendor> theQuery = currentSession.createQuery("from Vendor order by id asc", Vendor.class);
+//
+//		List<Vendor> vendors = theQuery.getResultList();
+		
+//		Criteria criteria = currentSession.createCriteria(Vendor.class);
+//		List vendors = criteria.list();
+		/*
+		 * CriteriaBuilder builder = currentSession.getCriteriaBuilder();
+		 * CriteriaQuery<Vendor> criteriaQuery = builder.createQuery(Vendor.class);
+		 * Root<Vendor> root = criteriaQuery.from(Vendor.class); Path<Object> venname =
+		 * root.get("sgdfg"); Path<Object> venemail = root.get("email");
+		 * 
+		 * criteriaQuery.select(builder.construct(Vendor.class, ));
+		 */
+		
+		
+		List<Vendor> vendors = currentSession.createCriteria(Vendor.class)
+							   .setFetchMode("vendorFiles", FetchMode.LAZY).list();	
+		System.out.println("vendors json: "+ vendors);
+		return vendors;
+	}
+
+}
